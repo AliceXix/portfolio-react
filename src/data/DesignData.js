ee83963584e1d3d8676894697df266aad72069c0
Alice Jost <alice.jost@outlook.de>
2023-08-14 19:09:36 +0200

    add designs data

---
+++ src/data/DesignData.js
@@ -0,0 +1,128 @@
+
+
+// --------------------------------------- Project List
+export const ProjectList = [
+    {
+        img: "Band page - d1.png",
+        title: "Band page",
+        description: "Design of a full page for a music group. Displayed here the main page.",
+        img_credit: "Stock image from the internet",
+        figma_url: ""
+    },
+    {
+        img: "Portfolio - d1.png",
+        title: "Portfolio",
+        description:
+            "Design of a portfolio with contact form for a tattoo artist. Displayed here the main view.",
+        img_credit: "Solanum Bean - Tattoo artist",
+        figma_url: ""
+    },
+    {
+        img: "Growth platform - d1.png",
+        title: "Growth platform",
+        description: "Design done in the context of a Hackathon of a webapplication to enable employees to help each other grow as well in their soft skills than in their hard skills. Displayed here the main view.",
+        img_credit: "Stock image from the internet",
+        figma_url: ""
+    },
+    {
+        img: "Request builder - d1.png",
+        title: "Request builder",
+        description: "Design of a simple webapplication to enable non-technical users to build an api request based on the possible parameters. Displayed here the second page.",
+        figma_url: ""
+    }
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+    {
+        img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
+        name: "JavaScript",
+    },
+    {
+        img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
+        name: "React classes",
+    },
+    {
+        img: "logos/react-hooks_logo.png",
+        name: "React Hooks"
+    },
+    {
+        img: "logos/Typescript_logo.svg",
+        name: "TypeScript"
+    },
+    {
+        img: "logos/nodejs_logo.svg",
+        name: "NodeJS"
+    },
+    {
+        img: "logos/expressjs_logo.png",
+        name: "ExpressJS"
+    },
+    {
+        img: "logos/mongodb-logo.png",
+        name: "MongoDB"
+    },
+    {
+        img: "logos/rest-api.png",
+        name: "RESTful API",
+    },
+    {
+        img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+        name: "HTML",
+    },
+    {
+        img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
+        name: "CSS",
+    },
+    {
+        img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+        name: "Git",
+    },
+    {
+        img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+        name: "Responsive",
+    },
+    {
+        img: "logos/agile_logo.jpg",
+        name: "Agile"
+    },
+    {
+        img: "logos/docker_logo.png",
+        name: "Docker"
+    },
+];
+
+export const stackListLearning = [
+    {
+        img: "logos/TDD_logo.webp",
+        name: "TDD"
+    },
+    {
+        img: "logos/docker_logo.png",
+        name: "Docker"
+    },
+    {
+        img: "logos/jest_logo.jpeg",
+        name: "Jest"
+    },
+    {
+        img: "logos/golang_logo.png",
+        name: "Golang"
+    },
+]
+
+export const stackListInterest = [
+    {
+        img: "logos/python.png",
+        name: "Python"
+    },
+    {
+        img: "logos/AWS_logo.png",
+        name: "AWS"
+    },
+    {
+        img: "logos/postgresql_logo.jpg",
+        name: "postgreSQL"
+    },
+]
