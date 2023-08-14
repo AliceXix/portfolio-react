1886254a6fb18e197cdf3e94e56fc4ddbb0d9a6c
Alice Jost <alice.jost@outlook.de>
2023-08-14 19:56:49 +0200

    added figma links to designs

--- src/data/DesignData.js
+++ src/data/DesignData.js
@@ -7,7 +7,7 @@ export const DesignList = [
         title: "Band page",
         description: "Design of a full page for a music group. Displayed here the main page.",
         img_credit: "Stock image from the internet",
-        figma_url: ""
+        figma_url: "https://www.figma.com/file/XDGpFLeGCgcfFSLwzZpN1e/Music-Band-Website---public?type=design&node-id=0%3A1&mode=design&t=gA6kUQPKZ6KnnEWa-1"
     },
     {
         img: "Portfolio - d1.png",
@@ -15,19 +15,19 @@ export const DesignList = [
         description:
             "Design of a portfolio with contact form for a tattoo artist. Displayed here the main view.",
         img_credit: "Solanum Bean - Tattoo artist",
-        figma_url: ""
+        figma_url: "https://www.figma.com/file/mCbdyJuTElI4FYw3kIIISK/Portfolio---public?type=design&node-id=0%3A1&mode=design&t=gA6kUQPKZ6KnnEWa-1"
     },
     {
         img: "Growth platform - d1.png",
         title: "Growth platform",
         description: "Design done in the context of a Hackathon of a webapplication to enable employees to help each other grow as well in their soft skills than in their hard skills. Displayed here the main view.",
         img_credit: "Stock image from the internet",
-        figma_url: ""
+        figma_url: "https://www.figma.com/file/rqKAI3SgGrHHjDWZMcEntZ/Knowledge-Growth-Platform---public?type=design&node-id=0%3A1&mode=design&t=gA6kUQPKZ6KnnEWa-1"
     },
     {
         img: "Request builder - d1.png",
         title: "Request builder",
         description: "Design of a simple webapplication to enable non-technical users to build an api request based on the possible parameters. Displayed here the second page.",
-        figma_url: ""
+        figma_url: "https://www.figma.com/file/tkRwnwANs7pfvCC0inm6Be/Request-Builder---public?type=design&node-id=2%3A8&mode=design&t=gA6kUQPKZ6KnnEWa-1"
     }
 ];
